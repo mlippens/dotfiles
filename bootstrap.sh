d73e94fbc696ef556088b006ab84d1d5e878e7e9
Michael Lippens <michael.lippens@barco.com>
2024-11-04 14:20:25 +0100

    fix: install nvim

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,9 +4,16 @@
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
-# install nvim 0.10
-curl https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz -0
-tar xzvf nvim-linux64.tar.gz
-echo "export PATH=$PATH:~/nvim-linux64/bin" >> ~/.bashrc
-source ~/.bashrc
+# install nvim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+rm -rf ~/nvim-linux64
+tar -C ~ -xzf nvim-linux64.tar.gz
+
+read -p "Do you want to add nvim as editor to your .bashrc? (y/n) " -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  echo "export PATH=$PATH:~/nvim-linux64/bin" >> ~/.bashrc
+  echo "alias vim=nvim" >> ~/.bashrc
+  echo "alias vi=nvim" >> ~/.bashrc
+  source ~/.bashrc
+fi
 
