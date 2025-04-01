e1ffaa10a4cfe75a0d43cb3eae68d26398b612d5
Michael Lippens <michael@yuso.com>
2025-04-01 11:55:03 +0200

    feat: add lazyvim

---
+++ lazyvim/.config/lazyvim/lua/plugins/avante.lua
@@ -0,0 +1,27 @@
+return {
+  "yetone/avante.nvim",
+  event = "VeryLazy",
+  version = false, -- Never set this value to "*"! Never!
+  opts = {
+    -- add any opts here
+    -- for example
+    provider = "gemini",
+    gemini = {
+      endpoint = "https://generativelanguage.googleapis.com/v1beta/models",
+      model = "gemini-2.5-pro-exp-03-25",
+      temperature = 0,
+      max_tokens = 4096,
+    },
+  },
+  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  build = "make",
+  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "stevearc/dressing.nvim",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    --- The below dependencies are optional,
+    "zbirenbaum/copilot.lua", -- for providers='copilot'
+  },
+}
