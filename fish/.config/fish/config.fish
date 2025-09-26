2dd6e6cc5e831a1bf484af213fb13d9880672721
Michael Lippens <michael@yuso.com>
2025-09-26 10:06:59 +0200

    updates

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -29,15 +29,6 @@ alias yal "aws sso login --profile=yuso"
 if test -e /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish
     source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish
 end
-#eval "$($HOME/miniforge3/bin/conda shell.fish hook)"
-
-fzf --fish | source
-zoxide init fish | source
-direnv hook fish | source
-fnm env --use-on-cd --shell fish | source
-
-# should be last
-starship init fish | source
 
 # Created by `pipx` on 2025-04-25 08:20:47
 set PATH $PATH /Users/michael/.local/bin
@@ -45,13 +36,23 @@ set PATH $PATH /Users/michael/.local/bin
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
 if test -f /opt/homebrew/Caskroom/miniforge/base/bin/conda
-    eval /opt/homebrew/Caskroom/miniforge/base/bin/conda "shell.fish" "hook" $argv | source
+    eval /opt/homebrew/Caskroom/miniforge/base/bin/conda "shell.fish" hook $argv | source
 else
     if test -f "/opt/homebrew/Caskroom/miniforge/base/etc/fish/conf.d/conda.fish"
         . "/opt/homebrew/Caskroom/miniforge/base/etc/fish/conf.d/conda.fish"
     else
-        set -x PATH "/opt/homebrew/Caskroom/miniforge/base/bin" $PATH
+        set -x PATH /opt/homebrew/Caskroom/miniforge/base/bin $PATH
     end
 end
 # <<< conda initialize <<<
 
+if status is-interactive
+    fzf --fish | source
+    zoxide init fish | source
+    direnv hook fish | source
+    fnm env --use-on-cd --shell fish | source
+
+    # should be last
+    starship init fish | source
+
+end
