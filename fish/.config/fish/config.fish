c721e4b41dc197c8120de711deae61b927112933
Michael Lippens <michael.lippens@robovision.eu>
2026-04-14 08:47:18 +0200

    updates

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,7 +6,6 @@ end
 # set up awscli tab completion
 complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
 
-
 fish_add_path ~/bin
 fish_add_path ~/.local/bin
 fish_add_path ~/.cargo/bin
@@ -16,10 +15,11 @@ if test -f ~/.config/fish/local.env
     source ~/.config/fish/local.env
 end
 
-
 alias g git
 alias vi nvim
 alias vim nvim
+alias k kubectl
+alias h helm
 alias p pnpm
 alias ls eza
 alias ll "eza -lah"
@@ -27,11 +27,11 @@ alias ll "eza -lah"
 set EDITOR nvim
 set -gx NVIM_APPNAME lazyvim
 
-function xc --description "Pipe to xc to copy to the system clipboard (uses xclip)"
-    xclip -selection clipboard $argv
+function xc --description "Pipe to xc to copy to the system clipboard (uses wl-copy)"
+    wl-copy $argv
 end
 function xpj --description "Pipe clipboard to jq (using xclip)"
-    xclip -selection clipboard -o | jq .
+    wl-copy -o | jq .
 end
 
 if test -e /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish
@@ -39,21 +39,11 @@ if test -e /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish
     fenv "source $HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
 end
 
-#eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-if test -f /home/michael/miniconda3/bin/conda
-    eval /home/michael/miniconda3/bin/conda "shell.fish" hook $argv | source
-else
-    if test -f "/home/michael/miniconda3/etc/fish/conf.d/conda.fish"
-        . "/home/michael/miniconda3/etc/fish/conf.d/conda.fish"
-    else
-        set -x PATH /home/michael/miniconda3/bin $PATH
-    end
-end
-# <<< conda initialize <<<
-
+# opencode
+fish_add_path /home/michael/.opencode/bin
+source ~/.safe-chain/scripts/init-fish.fish # Safe-chain Fish initialization script
 
 if status is-interactive
     fzf --fish | source
